0c3a0502406e7ab3aa0154d1d78b6b4c70cf0791
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-02-19 14:03:02 -0600

    added cloud config

---
+++ cloud-config-openstack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# template requires discovery token and public ssh keys
+# this hack below is needed to get IP address
+# tested on magellan
+ 
+until ! [[ -z $COREOS_PRIVATE_IPV4 ]]; do
+ 
+ echo "COREOS_PUBLIC_IPV4=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)" > /etc/environment
+ echo "COREOS_PRIVATE_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)" >> /etc/environment
+ source /etc/environment
+ 
+done
+ 
+cat << 'EOF' > /tmp/user_data.yml 
+#cloud-config
+
+
+coreos: 
+ etcd:
+   discovery: https://discovery.etcd.io/<token>
+   addr: \$private_ipv4:4001
+   peer-addr: \$private_ipv4:7001
+ fleet:
+   public-ip: \$private_ipv4
+ units:
+   - name: etcd.service
+     command: start
+   - name: fleet.service
+     command: start
+ssh_authorized_keys:
+  # include one or more SSH public keys
+  - <public ssh key>
+EOF
+ 
+sudo coreos-cloudinit --from-file=/tmp/user_data.yml
