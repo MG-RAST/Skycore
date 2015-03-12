6aad3391a39dcf1eb3c41b68b8a52e3d3eb800a6
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-03-12 09:48:55 -0700

    update awe server

--- register_docker_image_for_service_all.sh
+++ register_docker_image_for_service_all.sh
@@ -7,5 +7,5 @@ set -e
 curl -L http://127.0.0.1:4001/v2/keys/service_images/solr-m5nr/shock -XPUT -d value="shock.metagenomics.anl.gov/node/174bbd39-c80c-4473-964b-2b97a226d10c"
 curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/247d49e8-5699-4329-92cc-774a210b8dff"
 
-curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d value="shock.metagenomics.anl.gov/node/08be9757-bfdc-4c9e-961f-acb0ce1ecfef"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d value="shock.metagenomics.anl.gov/node/8ca81977-3670-4a9d-828d-80d578f59d41"
 curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-client/shock -XPUT -d value="shock.metagenomics.anl.gov/node/49186e36-6e4c-4606-b4ca-86c9ebf28c49"
