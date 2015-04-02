736bd3da5f49be7115c935fb5c456dd2519e4f00
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-04-02 17:04:09 -0500

    conf wrapper

---
+++ services/nginx/confd/run_confd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -x
+
+
+export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
+
+#or
+#export ETCD_ENDPOINT=172.17.42.1:4001
+
+
+confd -watch -node ${ETCD_ENDPOINT} -config-file /etc/confd/conf.d/nginx.toml 
+
