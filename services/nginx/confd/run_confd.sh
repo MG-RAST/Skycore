fedc6925b8380b5ead370e639fa4f231ebaaf7d9
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-04-02 17:20:13 -0500

    onetime

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -4,11 +4,18 @@ set -e
 set -x
 
 
+
 export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
 
 #or
 #export ETCD_ENDPOINT=172.17.42.1:4001
 
+
+
+confd -onetime -node ${ETCD_ENDPOINT} -config-file /etc/confd/conf.d/nginx.toml 
+
+# start nginx
+service nginx start &
 
 confd -watch -node ${ETCD_ENDPOINT} -config-file /etc/confd/conf.d/nginx.toml 
 
