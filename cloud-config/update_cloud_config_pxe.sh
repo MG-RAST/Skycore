a6f7fd0d4b017861539a9df26b5fe64400ae0361
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-04-15 17:03:30 -0500

    update template url

--- cloud-config/update_cloud_config_pxe.sh
+++ cloud-config/update_cloud_config_pxe.sh
@@ -10,7 +10,7 @@ NETWORK_INTERFACE=enp2s0f0
 # use template from local git repo or download if not exist
 if [ ! -e cloud-config-pxe.yml.template ] 
 then
-	wget --no-check-certificate https://raw.githubusercontent.com/wgerlach/Skycore/master/cloud-config-pxe.yml.template 
+	wget --no-check-certificate https://raw.githubusercontent.com/wgerlach/Skycore/master/cloud-config/cloud-config-pxe.yml.template 
 fi
 
 sed -e "s;%ssh_authorized_keys%;${PUBLIC_KEYS};g" -e "s;%network_interface%;${NETWORK_INTERFACE};g" -e "s;%discovery_token%;${DISCOVERY_TOKEN};g" cloud-config-pxe.yml.template > cloud-config-pxe.yml
