0980d4a3bfde7d1bee4ee6fcf0d09ecf9f8762ca
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-04-20 13:29:50 -0500

    awe-client update

--- register_docker_image_for_service_all.sh
+++ register_docker_image_for_service_all.sh
@@ -9,11 +9,11 @@ curl -L http://127.0.0.1:4001/v2/keys/service_images/solr-m5nr/shock -XPUT -d va
 # mg-rast-v4-web
 curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/247d49e8-5699-4329-92cc-774a210b8dff"
 
-# AWE server
+# AWE server (miminal image)
 curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server/shock -XPUT -d value="shock.metagenomics.anl.gov/node/a074d9fe-5c8e-4424-987b-d8ffc96da618"
 
-# AWE client
-curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-client/shock -XPUT -d value="shock.metagenomics.anl.gov/node/4cdfad56-ed3c-40d3-87cc-b97d4fb7588d"
+# AWE client (miminal image)
+curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-client/shock -XPUT -d value="shock.metagenomics.anl.gov/node/2b8e5837-e6e7-4483-bc0e-9ef3cb6b25ad"
 
 # MongoDB (for AWE server)
 curl -L http://127.0.0.1:4001/v2/keys/service_images/awe-server-mongodb/shock -XPUT -d value="shock.metagenomics.anl.gov/node/6dbd1649-0ad2-4c44-887b-aafeb02849fa"
