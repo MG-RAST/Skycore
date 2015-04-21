2e98fa6f29dddcbb44b151f214c3896e296806be
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-04-21 14:51:58 -0500

    test for git

--- cloud-config/update_cloud_config_pxe.sh
+++ cloud-config/update_cloud_config_pxe.sh
@@ -7,9 +7,15 @@ DISCOVERY_TOKEN=$(cat discovery_token.txt)
 NETWORK_INTERFACE=enp2s0f0
 
 
-# use template from local git repo or download if not exist
-if [ ! -e cloud-config-pxe.yml.template ] 
+# use template from local git repo or download
+git rev-parse --is-inside-work-tree > /dev/null 2>&1 
+IS_GIT_DIR=$?
+
+if [ ${IS_GIT_DIR} -eq 1 ] 
 then
+	git pull
+else 
+	rm -f cloud-config-pxe.yml.template
 	wget --no-check-certificate https://raw.githubusercontent.com/wgerlach/Skycore/master/cloud-config/cloud-config-pxe.yml.template 
 fi
 
