16f8a74fbc316be3e77ae7c825c11dba2c282852
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-01 15:46:46 -0500

    use repo for nginx

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -3,22 +3,20 @@
 
 set -x
 
-
-
 export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
-
+# usually => export ETCD_ENDPOINT=172.17.42.1:4001
+export CONF_DIR="/Skycore/services/nginx/confd"
 export TOML_FILE="/Skycore/services/nginx/confd/nginx.toml"
-
-#or
-#export ETCD_ENDPOINT=172.17.42.1:4001
+export CONFD_ARGS="-node ${ETCD_ENDPOINT} -config-file ${TOML_FILE} -confdir=${CONF_DIR}"
 
 
-
-confd -onetime -node ${ETCD_ENDPOINT} -config-file ${TOML_FILE} 
+# the first call often fails
+confd -onetime ${CONFD_ARGS}
 
 # start nginx
-service nginx start &
+
 set -e
 
-confd -watch -node ${ETCD_ENDPOINT} -config-file ${TOML_FILE} 
+confd -watch ${CONFD_ARGS} &
 
+nginx -c /Skycore/services/nginx/nginx.conf
