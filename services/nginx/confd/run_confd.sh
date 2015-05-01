dc17e9ff6413f8a500a31fc905c2232ef75e9af2
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-01 14:48:46 -0500

    use toml file from repo

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -7,16 +7,18 @@ set -x
 
 export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
 
+export TOML_FILE="/Skycore/services/nginx/confd/nginx.toml"
+
 #or
 #export ETCD_ENDPOINT=172.17.42.1:4001
 
 
 
-confd -onetime -node ${ETCD_ENDPOINT} -config-file /etc/confd/conf.d/nginx.toml 
+confd -onetime -node ${ETCD_ENDPOINT} -config-file ${TOML_FILE} 
 
 # start nginx
 service nginx start &
 set -e
 
-confd -watch -node ${ETCD_ENDPOINT} -config-file /etc/confd/conf.d/nginx.toml 
+confd -watch -node ${ETCD_ENDPOINT} -config-file ${TOML_FILE} 
 
