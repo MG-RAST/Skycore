ec5d179e2e5a2e85f0fdfe7a669d501aa82da71e
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-04 15:53:02 -0500

    TOML_FILE

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -6,8 +6,8 @@ set -x
 export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
 # usually => export ETCD_ENDPOINT=172.17.42.1:4001
 export CONF_DIR="/Skycore/services/nginx/confd"
-# does not work: export TOML_FILE="/Skycore/services/nginx/confd/nginx.toml"
-export CONFD_ARGS="-node ${ETCD_ENDPOINT} -confdir=${CONF_DIR}"
+export TOML_FILE="/Skycore/services/nginx/confd/conf.d/nginx.toml"
+export CONFD_ARGS="-node ${ETCD_ENDPOINT} -confdir=${CONF_DIR}" -config-file=${TOML_FILE}
 
 mkdir -p /etc/nginx/sites-enabled/
 
