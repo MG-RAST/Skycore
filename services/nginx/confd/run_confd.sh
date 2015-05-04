7737d81cd21c9435a5845b7df3d62d8f4cc0142d
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-04 15:45:05 -0500

    fix

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -6,8 +6,8 @@ set -x
 export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
 # usually => export ETCD_ENDPOINT=172.17.42.1:4001
 export CONF_DIR="/Skycore/services/nginx/confd"
-export TOML_FILE="/Skycore/services/nginx/confd/nginx.toml"
-export CONFD_ARGS="-node ${ETCD_ENDPOINT} -config-file ${TOML_FILE} -confdir=${CONF_DIR}"
+# does not work: export TOML_FILE="/Skycore/services/nginx/confd/nginx.toml"
+export CONFD_ARGS="-node ${ETCD_ENDPOINT} -confdir=${CONF_DIR}"
 
 
 # the first call often fails
