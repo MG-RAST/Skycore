5aaf3e420222308fefda0326928452e72f5525ad
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-04 17:09:45 -0500

    fix

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -4,8 +4,7 @@
 set -x
 
 if [ ! -e /usr/bin/docker ] ; then
-  docker run -t -i -v /var/run/docker.sock:/var/run/docker.sock --name mgrast_confd mgrast/nginx bash
-  curl https://get.docker.com/builds/Linux/x86_64/docker-1.6.0 > /usr/bin/docker && chmod +x /usr/bin/docker
+  curl https://get.docker.com/builds/Linux/x86_64/docker-1.6.0 && mv docker-1.6.0 /usr/bin/docker && chmod +x /usr/bin/docker
 fi
 
 export ETCD_ENDPOINT=$(route|grep default|awk '{print $2}'):4001
