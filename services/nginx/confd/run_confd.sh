7ee9187bf8aae8e2bc347ad95c394ff8980bf684
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-04 15:49:39 -0500

    mkdir

--- services/nginx/confd/run_confd.sh
+++ services/nginx/confd/run_confd.sh
@@ -9,14 +9,17 @@ export CONF_DIR="/Skycore/services/nginx/confd"
 # does not work: export TOML_FILE="/Skycore/services/nginx/confd/nginx.toml"
 export CONFD_ARGS="-node ${ETCD_ENDPOINT} -confdir=${CONF_DIR}"
 
+mkdir -p /etc/nginx/sites-enabled/
 
 # the first call often fails
 confd -onetime ${CONFD_ARGS}
-
+sleep 1
 # start nginx
 
 set -e
 
 confd -watch=false ${CONFD_ARGS} &
+
+sleep 1
 
 nginx -c /Skycore/services/nginx/nginx.conf
