2f87acb959f99d730a3f64d1dd9b522572258730
Wolfgang Gerlach <mail@wolfgang-gerlach.com>
2015-05-06 11:33:47 -0500

    no download here

--- services/nginx/confd/reload_nginx.sh
+++ services/nginx/confd/reload_nginx.sh
@@ -2,12 +2,4 @@
 set -e
 set -x
 
-
-export DOCKER_VERSION="1.5.0"
-if [ ! -e /usr/bin/docker ] ; then
-  curl -O https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}
-  mv docker-${DOCKER_VERSION} /usr/bin/docker 
-  chmod +x /usr/bin/docker
-fi
-
 /usr/bin/docker exec mgrast_nginx /usr/sbin/nginx -s reload -c /Skycore/services/nginx/nginx.conf
