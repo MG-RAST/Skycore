dc35733e11d92a155385b2ec996c9cc98284be1f
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-05-08 14:25:45 -0500

    update

--- register_docker_image_for_service_all.sh
+++ register_docker_image_for_service_all.sh
@@ -12,7 +12,7 @@ curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-confd/shock -XPUT -
 
 
 # solr-m5nr
-curl -L http://127.0.0.1:4001/v2/keys/service_images/solr-m5nr/shock -XPUT -d value="shock.metagenomics.anl.gov/node/174bbd39-c80c-4473-964b-2b97a226d10c"
+curl -L http://127.0.0.1:4001/v2/keys/service_images/solr-m5nr/shock -XPUT -d value="shock.metagenomics.anl.gov/node/82129683-8689-43bb-a9ec-4d4bdeb6248a"
 
 # mg-rast-v4-web
 curl -L http://127.0.0.1:4001/v2/keys/service_images/mg-rast-v4-web/shock -XPUT -d value="shock.metagenomics.anl.gov/node/247d49e8-5699-4329-92cc-774a210b8dff"
