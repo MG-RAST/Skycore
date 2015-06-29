c2cb8fd390d011c12da07d9c4d92494074aedf68
Wolfgang Gerlach <wgerlach@mcs.anl.gov>
2015-06-29 16:56:42 -0500

    test

--- skycore/skycore.go
+++ skycore/skycore.go
@@ -457,23 +457,24 @@ func (skyc *Skycore) get_dockerimage_shocknode_attributes(node_id string) (image
 		err = errors.New(fmt.Sprintf("error getting shock node: %s", strings.Join(node_response.Errs, ",")))
 	}
 
-	//docker_attr := node_response.Data.Attributes.(Dockerimage_attributes)
+	docker_attr2 := node_response.Data.Attributes.(Dockerimage_attributes)
 
-	//attr_json, err := json.Marshal(node_response.Data.Attributes) // ugly hack to get attributes (type: map[string]interface{}) into struct
+	attr_json, err := json.Marshal(node_response.Data.Attributes) // ugly hack to get attributes (type: map[string]interface{}) into struct
 
 	//var docker_attr Dockerimage_attributes
-	//err = json.Unmarshal(attr_json, &docker_attr)
-	//if err != nil {
-	//	return
-	//}
+	err = json.Unmarshal(attr_json, &docker_attr2)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("error Unmarshal: ", err.Error()))
+		return
+	}
 
 	//image_repository = docker_attr.Repository
 	//image_tag = docker_attr.Tag
 	//image_id = docker_attr.Id
 
-	image_repository = docker_attr.Repository
-	image_tag = docker_attr.Tag
-	image_id = docker_attr.Id
+	image_repository = docker_attr2.Repository
+	image_tag = docker_attr2.Tag
+	image_id = docker_attr2.Id
 
 	//attr_map, ok := node_response.Data.Attributes.(map[string]interface{}) // is of type map[string]interface{}
 
